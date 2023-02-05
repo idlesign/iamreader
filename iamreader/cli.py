2f4ecbc2b32a5ba759afca876fedff7a929ddc0f
idle sign <idlesign@yandex.ru>
2023-02-05 11:49:49 +0700

    CLI. Add basic video-related commands.

--- iamreader/cli.py
+++ iamreader/cli.py
@@ -1,31 +1,52 @@
 #!/usr/bin/env python
 import logging
+from pathlib import Path
 
 import click
 
-from iamreader import VERSION_STR
-from iamreader.utils import configure_logging
+from . import VERSION_STR
 from .exceptions import IamreaderException
 from .rc import RemoteControl, RemoteControlUi
+from .utils import configure_logging
+from .video import generate as video_generate
+from .video.services import YoutubeService
+
+PATH_CURRENT = Path.cwd()
 
 
 @click.group()
 @click.version_option(version=VERSION_STR)
-def entry_point():
+@click.option('--debug', help='Show debug messages', is_flag=True)
+def entry_point(debug):
     """iamreader command line utilities."""
+    configure_logging(logging.DEBUG if debug else None)
 
 
 @entry_point.command()
-@click.option('--debug', help='Show debug messages', is_flag=True)
-def rc(debug):
+def rc():
     """Launches a Remote Control UI for Audacity"""
-
-    if debug:
-        configure_logging(logging.DEBUG)
-
     window = RemoteControlUi(remote_control=RemoteControl())
     window.bind_shortcuts()
     window.loop()
+
+@entry_point.group()
+def video():
+    """Video related commands."""
+
+@video.command()
+def generate():
+    """Generates video from audio and text index file."""
+    video_generate(
+        path_in=PATH_CURRENT / 'in',
+        path_out=PATH_CURRENT / 'out',
+    )
+
+
+@video.command()
+@click.argument('service')
+def publish(service):
+    """Publish videos at a remote service."""
+    YoutubeService.publish()
 
 
 def main():
@@ -37,4 +58,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
